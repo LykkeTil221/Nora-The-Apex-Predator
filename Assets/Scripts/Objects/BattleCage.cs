4645554b2aea61cc72d23ab4da59ca5add5e15c3
Woolrich221 <ulrikschaeffer.2002@gmail.com>
2025-07-27 18:56:20 +0200

    feat: Finished the batle cage and added effects to spawners when they are spawning an enemy

--- Assets/Scripts/Objects/BattleCage.cs
+++ Assets/Scripts/Objects/BattleCage.cs
@@ -1,36 +1,68 @@
 using UnityEngine;
-
+using System.Collections.Generic;
+using System.Collections;
 public class BattleCage : MonoBehaviour
 {
-    [SerializeField] private EnemySpawner[] enemySpawners;
+    [SerializeField] private List <EnemySpawner> enemySpawners;
     [SerializeField] private GameObject TheBattleCage;
-
+    [SerializeField] private Animator animator;
     private void Start()
     {
         TheBattleCage.SetActive(false);
+        PlayerDamageManager.gameOver += ResetBattleCage;
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !TheBattleCage.activeSelf)
         {
             StartBattleCage();
+            animator.Play(0,0,0);
         }
     }
 
     private void StartBattleCage()
     {
         TheBattleCage.SetActive(true);
-        for (int i = 0; i < enemySpawners.Length; i++)
+        for (int i = 0; i < enemySpawners.Count; i++)
         {
             enemySpawners[i].isBattleCage = true;
+            enemySpawners[i].battleCage = this;
+            enemySpawners[i].StartSpawning();
         }
     }
 
-    private void Update()
+    public void RemoveSpawnerFromList(EnemySpawner spawner)
     {
-        if (enemySpawners == null)
+        enemySpawners.Remove(spawner);
+        CheckEnemyStatus();
+    }
+
+    private void CheckEnemyStatus()
+    {
+        if(enemySpawners.Count == 0)
         {
-            Destroy(gameObject);
+            animator.Play("BattleCageDefeated");
+            StartCoroutine(TurnOffBattleCage());
+        }
+    }
+
+    private IEnumerator TurnOffBattleCage()
+    {
+        yield return new WaitForSeconds(0.5f);
+        gameObject.SetActive(false);
+
+    }
+
+    private void ResetBattleCage(PlayerStateManager player)
+    {
+        TheBattleCage.SetActive(false);
+    }
+
+    public void ReAddSpawnerOnGameOver(EnemySpawner spawner)
+    {
+        if (!enemySpawners.Contains(spawner))
+        {
+            enemySpawners.Add(spawner);
         }
     }
 }
--- Assets/Scripts/Objects/EnemySpawner.cs
+++ Assets/Scripts/Objects/EnemySpawner.cs
@@ -4,28 +4,53 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] EnemyStateManager EnemyToSpawn;
     private EnemyStateManager Enemy;
-    public bool isBattleCage;
-
+    [HideInInspector] public bool isBattleCage;
+    public BattleCage battleCage;
+    private bool isSpawning;
+    private float timer;
+    [SerializeField] private float timeToSpawn;
+    [SerializeField] private ParticleSystem spawningParticle;
+    [SerializeField] private ParticleSystem finishedSpawningParticle;
+    [SerializeField] private int finishSpawnParticleCount;
     private void Start()
     {
         Enemy = Instantiate(EnemyToSpawn, transform);
+        Enemy.gameObject.SetActive(false);
+        StartSpawning();
+        PlayerCheckPoint.RespawnEnemies += StartSpawning;
+        
+
+        PlayerDamageManager.gameOver += GameOver;
+    }
+
+    private void Update()
+    {
+        if (!isSpawning) return;
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            SpawnEnemy();
+            spawningParticle.Stop();
+            spawningParticle.Clear();
+            finishedSpawningParticle.Emit(finishSpawnParticleCount);
+            isSpawning = false;
+        }
+    }
+
+    public void StartSpawning()
+    {
+        spawningParticle.Play();
+        Enemy.gameObject.SetActive(false);
+        timer = timeToSpawn;
+        isSpawning = true;
+    }
+
+    private void SpawnEnemy()
+    {
+
         Enemy.transform.rotation = transform.rotation;
         Enemy.spawner = this;
-        PlayerCheckPoint.RespawnEnemies += ReSpawnEnemy;
         Enemy.SwitchToNeutralState();
-    }
-
-    public void KillEnemy()
-    {
-        Enemy.transform.position = transform.position;
-        Enemy.transform.rotation = transform.rotation;
-        Enemy.gameObject.SetActive(false);
-        if (isBattleCage) { Destroy(gameObject); }
-    }
-
-    
-    public void ReSpawnEnemy()
-    {
         Enemy.transform.position = transform.position;
         Enemy.transform.rotation = transform.rotation;
         Enemy.gameObject.SetActive(true);
@@ -36,4 +61,21 @@ public class EnemySpawner : MonoBehaviour
         Enemy.currentAttack = 0;
         Enemy.currentAttackState = Enemy.Attacks[Enemy.currentAttack];
     }
+
+    public void KillEnemy()
+    {
+        Enemy.transform.position = transform.position;
+        Enemy.transform.rotation = transform.rotation;
+        Enemy.gameObject.SetActive(false);
+        if (isBattleCage) 
+        {
+            battleCage.RemoveSpawnerFromList(this);
+        }
+    }
+
+    private void GameOver(PlayerStateManager player)
+    {
+        battleCage.ReAddSpawnerOnGameOver(this);
+    }
+
 }
